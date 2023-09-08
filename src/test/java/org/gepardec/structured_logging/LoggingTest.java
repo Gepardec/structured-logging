0e66cb7627fbc654b60814054471f10c58caa718
Erhard Siegl <erhard.siegl@gepardec.com>
2023-09-08 13:52:37 +0200

    Experiments with log4j Layouts

---
+++ src/test/java/org/gepardec/structured_logging/LoggingTest.java
@@ -0,0 +1,27 @@
+package org.gepardec.structured_logging;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
+class LoggingTest {
+	
+	static Logger log = LoggerFactory.getLogger(LoggingTest.class);
+    
+    @Test
+	void simpleLog() throws Exception {
+		
+    	Person fritz = new Person("Fritz", 25);
+    	
+    	Jsonb jsonb = JsonbBuilder.create();
+    	String result = jsonb.toJson(fritz);
+    	
+    	log.info(result);
+
+	}
+}

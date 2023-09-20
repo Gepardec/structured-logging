5e7767747761314aef0600315970564e12b5cd56
Erhard Siegl <erhard.siegl@gepardec.com>
2023-09-20 13:17:03 +0200

    Try jboss-logging

---
+++ src/test/java/org/gepardec/structured_logging/JBossLoggingTest.java
@@ -0,0 +1,26 @@
+package org.gepardec.structured_logging;
+
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+
+class JBossLoggingTest {
+	
+	static Logger logger = Logger.getLogger(JBossLoggingTest.class);
+	
+    // setProperty "java.util.logging.manager" to "org.jboss.logmanager.LogManager"    
+    
+    @Test
+	void simpleLog() throws Exception {
+		
+    	Person fritz = new Person("Fritz", 25);
+    	org.jboss.logmanager.LogManager.getLogManager();
+        Jsonb jsonb = JsonbBuilder.create();
+        String result = jsonb.toJson(fritz);
+    	
+    	logger.info(result);
+    	
+	}
+}
